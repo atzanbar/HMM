0bde28565e67ce6c53f0f6586f4964ddcb3196e8
Alon zanbar <alon.zanbar@hp.com>
2017-11-13 17:12:14 +0200

    fix tuple issue

--- hmmscore.py
+++ hmmscore.py
@@ -1,5 +1,8 @@
 
 def get_score(word,tag,prev_tag,prev_prev_tag,ngram_counts,emission_counts,emission_counts_len):
-    pos_score = ngram_counts[tuple(prev_prev_tag,prev_tag,tag)]+1 / ngram_counts[tuple(prev_prev_tag,prev_tag)]+1
+    pos_score = 0.8*( ngram_counts[tuple([prev_prev_tag,prev_tag,tag])] / (ngram_counts[tuple([prev_prev_tag,prev_tag])]+1)) \
+    * 0.15*( ngram_counts[tuple([prev_tag,tag])] / (ngram_counts[tuple(prev_tag)]+1)) \
+    * 0.05*(ngram_counts[tuple(tag)]/emission_counts_len)
     escore = emission_counts[word]+1/emission_counts_len +1
     return  pos_score + escore
+
